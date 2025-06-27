cdbe36798df0690d3ab703e1e155250636a5ed44
PrathamAgarwal1 <prathamagarwalvikas@gmail.com>
2025-06-28 00:48:30 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Second Largest/second-largest.c
+++ Difficulty: Easy/Second Largest/second-largest.c
@@ -1,18 +1,17 @@
 int getSecondLargest(int *arr, int n) {
-    if(n<2){
-        return -1;
-    }
-    int largest=INT_MIN;
-    int slargest=INT_MIN;
-    for(int i=0;i<n;i++){
-        if(arr[i]>largest){
-            slargest=largest;
-            largest=arr[i];
+    if (n < 2) return -1;
+
+    int largest = INT_MIN;
+    int slargest = INT_MIN;
+
+    for (int i = 0; i < n; i++) {
+        if (arr[i] > largest) {
+            slargest = largest;
+            largest = arr[i];
+        } else if (arr[i] > slargest && arr[i] != largest) {
+            slargest = arr[i];
         }
-        else if(arr[i]>slargest && arr[i]!=largest){
-            slargest=arr[i];
-        }
     }
-    if(slargest==INT_MIN)return -1;
-    return slargest;
+
+    return (slargest == INT_MIN) ? -1 : slargest; 
 }
