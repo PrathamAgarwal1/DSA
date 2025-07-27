2986d4aa94d3f2ea48ce8b09fb3bcb7d33afb6e7
PrathamAgarwal1 <prathamagarwalvikas@gmail.com>
2025-07-28 02:47:11 +0530

    Time: 0 ms (100%), Space: 12.4 MB (67.25%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.cpp
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -8,7 +8,7 @@ public:
             if((mid==0||nums[mid]>nums[mid-1]) && (mid==n-1 ||nums[mid]>nums[mid+1])){
                 return mid;
             }
-            else if(mid<n-1 && nums[mid+1]>nums[mid]){//to avoid out of bounds error we did mid>0 coz we accesing mid-1
+            else if(mid<n-1 && nums[mid+1]>nums[mid]){//to avoid out of bounds error we did mid<n-1 coz we accesing mid+1
                 low=mid+1;
             }
             else{
