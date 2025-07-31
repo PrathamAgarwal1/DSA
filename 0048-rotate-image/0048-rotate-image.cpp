4a0253182f15445679da86bd57c102d766167a5d
PrathamAgarwal1 <prathamagarwalvikas@gmail.com>
2025-08-01 03:39:36 +0530

    Time: 0 ms (100%), Space: 10.2 MB (35.4%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -4,6 +4,14 @@ public:
         int n=matrix.size();
         for(int i=0;i<n;i++){
             for(int j=i+1;j<n;j++){//i+1 avoids redundant swaps and  with diagonals
+            /*
+            i = 0, j = 1 → swap(0,1) with (1,0) ✅
+            i = 1, j = 0 → swap(1,0) with (0,1) ❌ ← This undoes the first swap!
+            i = 0, j = 2 → swap(0,2) with (2,0)
+            i = 2, j = 0 → swap(2,0) with (0,2) ❌ Again, undoing it
+            i = 1, j = 2 → swap(1,2) with (2,1)
+            i = 2, j = 1 → swap(2,1) with (1,2) ❌
+            */
                 swap(matrix[i][j],matrix[j][i]);//finds transpose of matrix 
             }
         }
