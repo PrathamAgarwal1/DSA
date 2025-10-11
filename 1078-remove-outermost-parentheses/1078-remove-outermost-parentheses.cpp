0ec1d3765e3327c9f698c684e24c7115febafd30
Pratham Agarwal <prathamagarwalvikas@gmail.com>
2025-10-11 21:34:26 +0530

    Time: 0 ms (100%), Space: 9 MB (53.93%) - LeetHub

--- 1078-remove-outermost-parentheses/1078-remove-outermost-parentheses.cpp
+++ 1078-remove-outermost-parentheses/1078-remove-outermost-parentheses.cpp
@@ -1,25 +1,20 @@
 class Solution {
 public:
     string removeOuterParentheses(string s) {
-        vector <char> chars;
-        for (int i = 0; i < s.length(); ++i) {
-            chars.push_back(s[i]);
-        }
         string result="";
         int count=0;
-        int n=chars.size();
-        for(int i=0;i<n;i++){
-            char current=chars[i];
-            if(chars[i]=='('){
+        for(int i=0;i<s.size();i++){
+            if(s[i]=='('){
                 if(count>0){
-                    result+=current;
+                    result+=s[i];
                 }
                 count+=1;
+
             }
-            else if(chars[i]==')'){
+            else if(s[i]==')'){
                 count-=1;
                 if(count>0){
-                    result+=current;
+                    result+=s[i];
                 }
                 
             }
