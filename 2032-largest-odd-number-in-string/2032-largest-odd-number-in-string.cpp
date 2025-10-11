aa1eda88c91f94a550609474c9b873f9bde8eda7
Pratham Agarwal <prathamagarwalvikas@gmail.com>
2025-10-11 22:47:14 +0530

    Time: 4 ms (22.13%), Space: 17.7 MB (83.49%) - LeetHub

--- 2032-largest-odd-number-in-string/2032-largest-odd-number-in-string.cpp
+++ 2032-largest-odd-number-in-string/2032-largest-odd-number-in-string.cpp
@@ -1,14 +1,13 @@
 class Solution {
 public:
     string largestOddNumber(string num) {
-        
-        for(int i=num.size()-1;i>=0;i--){   
+        for(int i=num.size();i>=0;i--){
             char ch=num[i];
             if(ch=='1'||ch=='3'||ch=='5'||ch=='7'||ch=='9'){
-                return num.substr(0,i+1);//i+1 is the length
+                return num.substr(0,i+1);//returns the substring from index 0 up to index i. peeche se start kia taaki longest odd number jo hota voh return hojaata
             }
         }
         return "";
-        
+    
     }
 };
