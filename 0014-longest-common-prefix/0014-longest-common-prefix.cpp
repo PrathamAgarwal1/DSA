940fb1a48508ebbad3afc66e94e21b4fb9dc9543
Pratham Agarwal <prathamagarwalvikas@gmail.com>
2025-10-12 18:55:40 +0530

    Time: 0 ms (100%), Space: 11.9 MB (68.67%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.cpp
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -1,19 +1,17 @@
 class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs) {
-        string result="";
-        for(int i=0;i<strs[0].size();i++){
-            char c=strs[0][i];
-            for(int j=1;j<strs.size();j++){
-                if(i>strs[j].size()||strs[j][i]!=c){
-                    return result ;
-                }
-                
+        string ans="";
+        sort(strs.begin(),strs.end());
+        int n=strs.size();
+        string first=strs[0];
+        string last=strs[n-1];//Why only compare 1st and last after sorting most that differ is first and last
+        for(int i=0;i<min(first.size(),last.size());i++){//jiska size chota hoga utne tak hi traverse krne ka matlab hai
+            if(first[i]!=last[i]){
+                return ans;
             }
-            result+=c;
-
-
+            ans+=first[i];
         }
-        return result;
+        return ans;
     }
 };
