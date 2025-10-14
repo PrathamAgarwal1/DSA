250ac547150e23c7963adf18f98644067ec86751
Pratham Agarwal <prathamagarwalvikas@gmail.com>
2025-10-14 20:50:09 +0530

    Time: 0 ms (100%), Space: 8.2 MB (61.39%) - LeetHub

--- 0812-rotate-string/0812-rotate-string.cpp
+++ 0812-rotate-string/0812-rotate-string.cpp
@@ -2,8 +2,8 @@ class Solution {
 public:
     bool rotateString(string s, string goal) {
         if(s.size()!=goal.size())return false;
-        string check=s+s;
-        if(check.find(goal)<check.size()){
+        string rotationcheck=s+s;
+        if(rotationcheck.find(goal)<=rotationcheck.size()){
             return true;
         }
         return false;
