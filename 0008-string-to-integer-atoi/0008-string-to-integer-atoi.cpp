f1d8aedf5defa09aa457fa13a305c98c2ab08d6b
Pratham Agarwal <prathamagarwalvikas@gmail.com>
2025-10-16 13:46:40 +0530

    Time: 0 ms (100%), Space: 9.1 MB (54.75%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
@@ -1,33 +1,26 @@
 class Solution {
 public:
     int myAtoi(string s) {
-        int i = 0;
-        int sign = 1;
-        long result = 0;
-        while(s[i]==32){//for whitespace
+        int i=0;
+        int sign=1;
+        long long result=0;
+        while(s[i]==' '){
             i++;
         }
-        if(s[i]==43 || s[i]==45){//for sign
-            if(s[i]==43){
-                sign=1;
-                
-            }
-            else{
-                sign=-1;
-
-            }
+        if(s[i]=='+'){
+            sign=1;
             i++;
         }
-        while(s[i]>=48 && s[i]<=57){
-            int digit=s[i]-48;
-            result=(result*10)+digit;
-            if(sign==1 && result>INT_MAX) return INT_MAX;
-            if(sign==-1 && -result<INT_MIN) return INT_MIN;
+        else if(s[i]=='-'){
+            sign=-1;
             i++;
         }
-        
+        while(s[i]>='0' && s[i]<='9'){
+            result=(result*10)+(s[i]-'0');
+            if(sign*result > INT_MAX) return INT_MAX;
+            if(sign*result <INT_MIN) return INT_MIN;
+            i++;
+        }
         return sign*result;
     }
-        
-    
 };
